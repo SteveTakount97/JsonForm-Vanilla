410fd724fc2c36a46a7d2ae4fca3bf50d2d030ec
steve <stevetak@outlook.fr>
2025-03-30 00:54:25 +0100

    edit style and gitpage

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   // Configuration pour le déploiement sur GitHub Pages
-  base: '/', 
+  base: '/JsonForm-Vanilla', 
 
   plugins: [react()],
 
